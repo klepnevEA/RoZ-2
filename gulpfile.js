193b09b498fca6b9c5613d7fca2d6c2ad4af60f3
Klepnev <klepnev-ea@yandex.ru>
2017-04-10 14:40:05 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+global.$ = {
+  package: require('./package.json'),
+  config: require('./gulp/config'),
+  path: {
+    task: require('./gulp/paths/tasks.js')
+    // jsFoundation: require('./gulp/paths/js.foundation.js'),
+    // cssFoundation: require('./gulp/paths/css.foundation.js'),
+    // app: require('./gulp/paths/app.js')
+  },
+  gulp: require('gulp'),
+  rimraf: require('del'),
+  merge: require('merge-stream'),
+  browserify : require('browserify'),
+  source : require('vinyl-source-stream'),
+  buffer : require('vinyl-buffer'),
+  babel : require('babelify'),
+  browserSync: require('browser-sync').create(),
+  gp: require('gulp-load-plugins')()
+};
+
+$.path.task.forEach(function(taskPath) {
+  require(taskPath)();
+});
+
+$.gulp.task('default', $.gulp.series(
+  'clean',
+  $.gulp.parallel(
+    'less',
+    'copy:content',
+    'copy:dist',
+    'copy:fonts',
+    'copy:html',
+    'copy:image',
+    'copy:scripts'
+  ),
+  $.gulp.parallel(
+    'watch',
+    'serve'
+  )
+));
---
+++ source/scripts/scripts.js
@@ -0,0 +1,19 @@
+// Рейтинг со звездами
+
+$(function(){
+	
+	$('#rating_2').rating({
+		fx: 'half',
+        image: '../img/stars.png',
+        loader: '../img/ajax-loader.gif',
+		url: 'rating.php',
+        callback: function(responce){
+            
+            this.vote_success.fadeOut(2000);
+            
+            alert('Общий бал: '+this._data.val);
+        }
+	});
+	
+})
+// --/Рейтинг со звездами
