ddc85877f4c982a0f7cc26f387b7c4c6dbafa894
Klepnev <klepnev-ea@yandex.ru>
2017-07-18 18:00:47 +0300

    Кучка мелких-мелких правок

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -1,29 +1,42 @@
 // Рейтинг со звездами
 
-$(function(){
+// $(function(){
 	
-	$('#rating_2').rating({
-		fx: 'half',
-        image: '../img/stars.png',
-        loader: '../img/ajax-loader.gif',
-		url: 'rating.php',
-        callback: function(responce){
+// 	$('#rating_2').rating({
+// 		fx: 'half',
+//         image: '../img/stars.png',
+//         loader: '../img/ajax-loader.gif',
+// 		url: 'rating.php',
+//         callback: function(responce){
             
-            this.vote_success.fadeOut(2000);
+//             this.vote_success.fadeOut(2000);
             
-            alert('Общий бал: '+this._data.val);
-        }
-	});
+//             alert('Общий бал: '+this._data.val);
+//         }
+// 	});
 	
-})
+// })
 // --/Рейтинг со звездами
 
 
 $(document).ready(function() {
-    $('.teatcher__title').click(function() {
-        $(this).addClass('active')
-        $('.infoblock').addClass('active')
-    });
+    // $('.teatcher__title').click(function() {
+    //     $(this).addClass('active')
+    //     $('.infoblock').addClass('active')
+    // });
+
+
+    $('.grantz__tabs-elem .btn').on('click' ,function() {
+        var that = $(this),
+            data_tab = that.data('grantz-tab');
+            
+            $('.grantz__tabs-elem .btn').removeClass('active');
+            that.addClass('active');
+
+            $('.grantz-tabs').removeClass('active');
+            $('#'+data_tab).addClass('active');
+
+    })
 }); 
 
 
