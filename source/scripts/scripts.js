66887fa8f07f372fb83b7a47b8628c80f5d08b77
romuald <rarsenev@ustech.ru>
2017-10-25 13:43:37 +0300

    дропдаун для слепых

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -1,22 +1,3 @@
-// Рейтинг со звездами
-
-// $(function(){
-	
-// 	$('#rating_2').rating({
-// 		fx: 'half',
-//         image: '../img/stars.png',
-//         loader: '../img/ajax-loader.gif',
-// 		url: 'rating.php',
-//         callback: function(responce){
-            
-//             this.vote_success.fadeOut(2000);
-            
-//             alert('Общий бал: '+this._data.val);
-//         }
-// 	});
-	
-// })
-// --/Рейтинг со звездами
 
 
 $(document).ready(function() {
@@ -52,6 +33,127 @@ $(document).ready(function() {
         }
     })
 
-}); 
 
 
+
+
+//скрипты для главной стр для слабовидящих
+
+$('.menu-viz .menu-viz__item').click(function() {
+    $('.menu-viz__item').removeClass('active');
+    $(this).addClass('active');
+});
+$('.controlpanel__item_font .controlpanel__item-btn').click(function() {
+    $('.controlpanel__item_font .controlpanel__item-btn').removeClass('active');
+    $(this).addClass('active');
+});
+$('.controlpanel__list_color .controlpanel__item-btn').click(function() {
+    $('.controlpanel__item_color .controlpanel__item-btn').removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.controlpanel__item_font .controlpanel__item-btn').click(function() {
+    $font = $(this).attr('id');
+    $('body').removeClass('fontSizeSmall');
+    $('body').removeClass('fontSizeMedium');
+    $('body').removeClass('fontSizeBig');
+    $('body').addClass($font);
+ });
+
+$('.controlpanel__item_color .controlpanel__item-btn').click(function() {
+    $color = $(this).attr('id');
+    $('body').removeClass('ColorBlack');
+    $('body').removeClass('ColorBlue');
+    $('body').addClass($color);
+ });
+
+$('.management__comments__button-play a').text('Перейти к новости');
+$('.management__comments__date .icon-clock').text('Опубликовано:');
+$('.activities-calendar .link-all__link').text('Перейти ко всем мероприятиям');
+$('.news .link-all__link').text('Перейти ко всем новостям');
+$('.news__data .icon-clock').text('Опубликовано: ');
+
+
+$(".viz-modal__box input[type='radio']").change(function() {
+    if($(".viz-modal__box #radio-1").prop("checked")) { 
+            console.log('белый');
+            $('body').removeClass('ColorBlack');
+            $('body').removeClass('ColorBlue');
+            $('.controlpanel__item_color .controlpanel__item-btn').removeClass('active');
+            $('.controlpanel__list_color #ColorWhite').addClass('active');
+
+    }
+    if($(".viz-modal__box #radio-2").prop("checked")) { 
+            console.log('черный');
+            $('body').removeClass('ColorBlue');
+            $('body').addClass('ColorBlack');
+
+            $('.controlpanel__item_color .controlpanel__item-btn').removeClass('active');
+            $('.controlpanel__list_color #ColorBlack').addClass('active');
+            
+            
+    }
+    if($(".viz-modal__box #radio-3").prop("checked")) { 
+            console.log('синий');
+            $('body').removeClass('ColorBlack');
+            $('body').addClass('ColorBlue');
+            $('.controlpanel__item_color .controlpanel__item-btn').removeClass('active');
+            $('.controlpanel__list_color #ColorBlue').addClass('active');
+    }
+});
+
+        
+
+//модалки задаем кнопке class = 'modal" и id, cамой модалкe даем класс равный id 
+
+$('.modall').click(function() {
+    $id = this.id;
+    $('.viz-modal' + '.'+ $id ).addClass('active');
+    $('html, body').scrollTop( 0 ); 
+
+});
+
+$('.viz-modal__close').click(function() {
+    $(this).closest('.viz-modal').removeClass('active');
+});
+$('.viz-modal .viz-modal__btns .btn').click(function() {
+    $(this).closest('.viz-modal').removeClass('active');
+});
+$('#mod-4').click(function() {
+    $('.mod-3').removeClass('active');
+});
+
+
+
+
+
+$('.viz-modal__box-btns_cer .btn').click(function() {
+    $('.viz-modal__box-btns_cer .btn').removeClass('active');
+    $(this).addClass('active');
+
+    $Letter = $(this).attr('id');
+    $('body').removeClass('LetterSpacingSmall');
+    $('body').removeClass('LetterSpacingMedium');
+    $('body').removeClass('LetterSpacingBig');
+    $('body').addClass($Letter);
+});
+$('.viz-modal__box-btns_font .btn').click(function() {
+    $('.viz-modal__box-btns_font .btn').removeClass('active'); 
+    $(this).addClass('active');
+
+    $fontf = $(this).attr('id');
+    $('body').removeClass('FontFamilyArial');
+    $('body').removeClass('FontFamilyTimesNewRoman');
+    $('body').addClass($fontf);
+});
+
+//dropdown
+
+    $(".dropdown .dropdown__box, .dropdown .dropdown__arrow-down").click(function() {
+        $(".dropdown .dropdown__btn").toggleClass('active');
+    });
+
+    $(".m-select .mdi-menu-down").click(function() {
+        $(".m-select .m-select__block").toggle();
+    });
+});
