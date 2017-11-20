bf2f9b76e88040775c285c7c4f942ea419f53460
Romuald <rarsenev@usetech.ru>
2017-11-20 17:48:02 +0300

    страница лк-лектор-2-моб

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -2,13 +2,52 @@
 $(function(){
 	$(".slider").slick({
 	    dots: true,
-	    arrows: true
+	    arrows: true,
+        responsive: [
+            {
+                breakpoint: 2000,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+				}
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+
+                }
+            }
+        ]
 	});
 
 
 	$(".slider-without-arrows").slick({
-	    dots: true,
-	    arrows: false
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 2000,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+
+                }
+            }
+        ]
 	});
 
 })
+
